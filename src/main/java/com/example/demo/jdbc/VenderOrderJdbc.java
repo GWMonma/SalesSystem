2bf4a2434a96a4311ad90071802447b2e1c77fe3
GWMonma <83321035+GWMonma@users.noreply.github.com>
2021-06-11 10:32:49 +0900

    Update VenderOrderJdbc.java

--- src/main/java/com/example/demo/jdbc/VenderOrderJdbc.java
+++ src/main/java/com/example/demo/jdbc/VenderOrderJdbc.java
@@ -24,4 +24,24 @@ public class VenderOrderJdbc {
 		return "発注が完了しました。";
 	}
 	
+	
+//仕入管理　検索機能
+		public List<VenderOrderModel> getVenderOrder(String item_name) {
+	
+	String sql = "SELECT * from venderorder where item_name like ?";
+	List<Map<String, Object>>venderOrderList = jdbcTemplate.queryForList(sql,'%'+item_name+'%');
+	List<VenderOrderModel> list = new ArrayList<>();
+	for(Map<String,Object> str1 : venderOrderList) {
+		VenderOrderModel vom = new VenderOrderModel();
+		vom.setVender_order_no((int)str1.get("vender_order_no"));
+		vom.setItem_name((String)str1.get("item_name"));
+		vom.setItem_product_no((String)str1.get("item_product_no"));
+		vom.setItem_buy_count((int)str1.get("item_buy_count"));
+		vom.setTotal_price((int)str1.get("total_price"));
+		vom.setArrival_due_date((Date)str1.get("arrival_due_date"));
+		list.add(vom);
+		}
+	return list;
+	}
+	
 }
