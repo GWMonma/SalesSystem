545d5e30c36e57161f3522a6ff81a7f4b04cd801
GWMonma <83321035+GWMonma@users.noreply.github.com>
2021-06-14 15:52:20 +0900

    Add files via upload

---
+++ src/main/java/com/example/demo/jdbc/ClientOrderJdbc.java
@@ -0,0 +1,53 @@
+package com.example.demo.jdbc;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.model.ClientOrderModel;
+@Component
+public class ClientOrderJdbc {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	//仕入れ管理　仕入日予定日の更新
+	public String shipmentDateUpdate(int client_order_no,Date shipment_due_date) {
+		try {
+			this.jdbcTemplate.update("update clientorder set shipment_due_date= ? where client_order_no= ?",shipment_due_date,client_order_no);
+		}catch(Exception ex) {
+			return "エラーが発生しました。";
+		}
+		return "更新が完了しました。";
+	}
+	
+	
+	//受注履歴を取得
+		public ArrayList<ClientOrderModel> getClientOrderLog(String searchWord){
+			ArrayList<ClientOrderModel> returnList = new ArrayList<ClientOrderModel>();
+			try {
+				String sql = "SELECT * FROM clientorder WHERE item_name LIKE ?";
+				List<Map<String, Object>> itemDataList = jdbcTemplate.queryForList(sql, '%'+searchWord+'%');
+				//格納する
+				for(Map<String, Object> mapData : itemDataList) {
+					ClientOrderModel returnData = new ClientOrderModel();
+					returnData.setClient_order_no((int)mapData.get("client_order_no"));
+					returnData.setItem_name((String)mapData.get("item_name"));
+					returnData.setItem_product_no((String)mapData.get("item_product_no"));
+					returnData.setItem_buy_count((int)mapData.get("item_buy_count"));
+					returnData.setTotal_price((int)mapData.get("total_price"));
+					returnData.setItem_buy_date((Date)mapData.get("item_buy_date"));
+					returnData.setShipment_due_date((Date)mapData.get("shipment_due_date"));
+					returnList.add(returnData);
+				}
+			}catch(Exception ex) {
+			
+			}
+			return returnList;
+		}
+
+}
