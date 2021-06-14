c743d33b6ce0ba3d47f3695103d7700dd7ddcc76
OkajimaAyaka <83321115+OkajimaAyaka@users.noreply.github.com>
2021-06-14 17:25:21 +0900

    Update InventoryManagementController.java

--- src/main/java/com/example/demo/controller/InventoryManagementController.java
+++ src/main/java/com/example/demo/controller/InventoryManagementController.java
@@ -23,6 +23,9 @@ public class InventoryManagementController {
 
 	@Autowired
 	private ClientOrderJdbc clientOrderJdbc;
+	
+	@Autowired
+	ItemLogic itemLogic;
 
 
 
@@ -130,7 +133,26 @@ public class InventoryManagementController {
 	        return "html/ShipmentManagement";
 	    }
 
-
+/*入荷管理画面へ遷移*/
+    @RequestMapping("ArrivalManagement")
+	public String inventory(Model model) {
+    	return "html/ArrivalManagement";
+	}
+    
+    @RequestMapping("ArrivalDateUpdate")
+    public String arrivalDueDateUpdate(@RequestParam("itemNo") String itemNo, Model model) {
+    	//intに変換可能か確認する
+    	String noConfirmationStr = itemLogic.inputConfirmation(itemNo);
+    	if(noConfirmationStr.equals("数値を入力してください。")){
+    		model.addAttribute("resultText", noConfirmationStr);
+    		return "html/ArrivalManagement";
+    	}
+    	
+    	//入力された番号が存在するか、既に確定されているか確認し、入荷確定処理を行う。
+    	String resultText = itemLogic.checkItemNoLogic(Integer.parseInt(itemNo));
+    	model.addAttribute("resultText", resultText);
+        return "html/ArrivalManagement";
+    }
 
 
 
