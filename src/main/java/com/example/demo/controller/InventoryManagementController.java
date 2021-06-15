44002b5552372bcc732a5a4332ef9ea8aae39506
OkajimaAyaka <83321115+OkajimaAyaka@users.noreply.github.com>
2021-06-15 11:37:12 +0900

    InventoryManagementController.javaの編集。
    InventoryManagementController.javaの編集を行いました。

--- src/main/java/com/example/demo/controller/InventoryManagementController.java
+++ src/main/java/com/example/demo/controller/InventoryManagementController.java
@@ -13,163 +13,194 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.example.demo.jdbc.ClientOrderJdbc;
 import com.example.demo.jdbc.ItemJdbc;
 import com.example.demo.logic.ItemLogic;
+import com.example.demo.logic.VenderOrderLogic;
 import com.example.demo.model.ClientOrderModel;
 import com.example.demo.model.InventoryModel;
 import com.example.demo.model.LoginModel;
+import com.example.demo.model.VenderOrderModel;
 
 /*在庫管理関係のコントローラー*/
 @Controller
 public class InventoryManagementController {
 	String message;
-	 @Autowired
-	 HttpSession session;
+	@Autowired
+	HttpSession session;
 
+	@Autowired
+	private ItemJdbc itemJdbc;
 
-		@Autowired
-		private ItemJdbc itemJdbc;
-
-		@Autowired
-		private ClientOrderJdbc clientOrderJdbc;
-
-		 @Autowired
-		 ItemLogic itemLogic;
+	@Autowired
+	private ClientOrderJdbc clientOrderJdbc;
 	
-		 /*入荷管理画面へ遷移*/
-	   	@RequestMapping("ArrivalManagement")
-	    	public String arrival(Model model) {
-	    		if(session.getAttribute("data") == null) {
-	    			message ="IDとパスワードを入力してください";
-	    	    		model.addAttribute("indexForm", new LoginModel());
-	    	    		model.addAttribute("message" , message);
-	    	    		return "Login";
-	    		}
+	@Autowired
+	ItemLogic itemLogic;
+	
+	@Autowired
+	VenderOrderLogic venderOrderLogic;
 
-	        	return "html/ArrivalManagement";
+
+
+	 /*入荷管理画面へ遷移*/
+   	@RequestMapping("ArrivalManagement")
+    	public String arrival(Model model) {
+    		if(session.getAttribute("data") == null) {
+    			message ="IDとパスワードを入力してください";
+    	    		model.addAttribute("indexForm", new LoginModel());
+    	    		model.addAttribute("message" , message);
+    	    		return "Login";
+    		}
+
+        	return "html/ArrivalManagement";
+    	}
+
+	    /*出荷管理画面へ遷移*/
+	    @RequestMapping("ShipmentManagement")
+	    public String shipment(Model model) {
+	    	if(session.getAttribute("data") == null) {
+	    		message ="IDとパスワードを入力してください";
+	    	    model.addAttribute("indexForm", new LoginModel());
+	    	    model.addAttribute("message" , message);
+	    	    return "Login";
 	    	}
 
-		    /*出荷管理画面へ遷移*/
-		    @RequestMapping("ShipmentManagement")
-		    public String shipment(Model model) {
-		    	if(session.getAttribute("data") == null) {
-		    		message ="IDとパスワードを入力してください";
-		    	    model.addAttribute("indexForm", new LoginModel());
-		    	    model.addAttribute("message" , message);
-		    	    return "Login";
-		    	}
-
-		        return "html/ShipmentManagement";
-		    }
-
-		    /*在庫管理画面へ遷移*/
-		    @RequestMapping("InventoryManagement")
-		    public String inventory(Model model) {
-			if(session.getAttribute("data") == null) {
-				message ="IDとパスワードを入力してください";
-			    model.addAttribute("indexForm", new LoginModel());
-			    model.addAttribute("message" , message);
-			    return "Login";
-			}
-		        return "html/InventoryManagement";
-		    }
-
-		    /*棚卸・在庫調整画面へ遷移*/
-		    @RequestMapping("InventoryAdjustment")
-		    public String  	inventoryadjustment(Model model) {
-			if(session.getAttribute("data") == null) {
-				message ="IDとパスワードを入力してください";
-			    model.addAttribute("indexForm", new LoginModel());
-			    model.addAttribute("message" , message);
-			    return "Login";
-			}
-		        return "html/InventoryAdjustment";
-		    }
-
-		    @Autowired
-			private ItemLogic ItemLogic;
-
-		    @RequestMapping("InventrySearch")
-			public String inventoryadjustmentsearch(@RequestParam("item_name") String searchWord, Model model) {
-				ArrayList<InventoryModel> returnList = ItemLogic.getInventoryLog(searchWord);
-				//検索結果を渡す
-				if(returnList.size()==0){
-					model.addAttribute("searchList", null);
-				}else{
-					model.addAttribute("searchList", returnList);
-				}
-				model.addAttribute("resultText", "検索結果"+returnList.size()+"件");
-				return "html/InventoryAdjustment";
-			}
-
-
-	  //データベースと照合して受注情報を表示(フォーム)
-	    @RequestMapping("OrderSearchForm")
-	    public String orderSearch1(@RequestParam("item_name") String item_name, Model model){
-			ArrayList<ClientOrderModel> list = clientOrderJdbc.getClientOrderLog(item_name);
-			model.addAttribute("clientOrderList",list);
-			model.addAttribute("item_name", item_name);
 	        return "html/ShipmentManagement";
 	    }
 
+	    /*在庫管理画面へ遷移*/
+	    @RequestMapping("InventoryManagement")
+	    public String inventory(Model model) {
+		if(session.getAttribute("data") == null) {
+			message ="IDとパスワードを入力してください";
+		    model.addAttribute("indexForm", new LoginModel());
+		    model.addAttribute("message" , message);
+		    return "Login";
+		}
+	        return "html/InventoryManagement";
+	    }
 
-		    //出荷予定日の更新
-		 @RequestMapping("ShipmentManagementDateUpdate")
-		    public String shipmentManagementDateUpdate(@RequestParam("order_no") String client_order_no,@RequestParam("date_update") String shipment_due_date ,@RequestParam("item_name") String item_name, Model model){
+	    /*棚卸・在庫調整画面へ遷移*/
+	    @RequestMapping("InventoryAdjustment")
+	    public String  	inventoryadjustment(Model model) {
+		if(session.getAttribute("data") == null) {
+			message ="IDとパスワードを入力してください";
+		    model.addAttribute("indexForm", new LoginModel());
+		    model.addAttribute("message" , message);
+		    return "Login";
+		}
+	        return "html/InventoryAdjustment";
+	    }
 
-			 	int totalItemNo = itemJdbc.getItemDataList().size();
-		    	String resultText = null;
-		    	int no = -1;
+	    @Autowired
+		private ItemLogic ItemLogic;
 
-		    	try {
-					no = Integer.parseInt(client_order_no);
-				}catch(Exception ex){
-					model.addAttribute("resultText","数値を入力してください。");
-			        return "html/ShipmentManagement";
-				}
+	    @RequestMapping("InventrySearch")
+		public String inventoryadjustmentsearch(@RequestParam("item_name") String searchWord, Model model) {
+			ArrayList<InventoryModel> returnList = ItemLogic.getInventoryLog(searchWord);
+			//検索結果を渡す
+			if(returnList.size()==0){
+				model.addAttribute("searchList", null);
+			}else{
+				model.addAttribute("searchList", returnList);
+			}
+			model.addAttribute("resultText", "検索結果"+returnList.size()+"件");
+			return "html/InventoryAdjustment";
+		}
 
-		    	if(totalItemNo<no) {
-		    		resultText = "入力された番号は存在しません。";
-		    	}else{
-		    		resultText = clientOrderJdbc.shipmentDateUpdate(no,shipment_due_date);
-		    	}
-		    	ArrayList<ClientOrderModel> list = clientOrderJdbc.getClientOrderLog(item_name);
-		    	model.addAttribute("clientOrderList",list);
-		    	model.addAttribute("item_name", item_name);
-				model.addAttribute("resultText",resultText);
+
+  //データベースと照合して受注情報を表示(フォーム)
+    @RequestMapping("OrderSearchForm")
+    public String orderSearch1(@RequestParam("item_name") String item_name, Model model){
+		ArrayList<ClientOrderModel> list = clientOrderJdbc.getClientOrderLog(item_name);
+		model.addAttribute("clientOrderList",list);
+		model.addAttribute("item_name", item_name);
+        return "html/ShipmentManagement";
+    }
+
+
+	    //出荷予定日の更新
+	 @RequestMapping("ShipmentManagementDateUpdate")
+	    public String shipmentManagementDateUpdate(@RequestParam("order_no") String client_order_no,@RequestParam("date_update") String shipment_due_date ,@RequestParam("item_name") String item_name, Model model){
+
+		 	int totalItemNo = itemJdbc.getItemDataList().size();
+	    	String resultText = null;
+	    	int no = -1;
+
+	    	try {
+				no = Integer.parseInt(client_order_no);
+			}catch(Exception ex){
+				model.addAttribute("resultText","数値を入力してください。");
 		        return "html/ShipmentManagement";
-		    }
+			}
 
-		    @RequestMapping("ArrivalDateUpdate")
-		    public String arrivalDueDateUpdate(@RequestParam("itemNo") String itemNo, Model model) {
-		    	//intに変換可能か確認する
-		    	String noConfirmationStr = itemLogic.inputConfirmation(itemNo);
-		    	if(noConfirmationStr.equals("数値を入力してください。")){
-		    		model.addAttribute("resultText", noConfirmationStr);
-		    		return "html/ArrivalManagement";
-		    	}
+	    	if(totalItemNo<no) {
+	    		resultText = "入力された番号は存在しません。";
+	    	}else{
+	    		resultText = clientOrderJdbc.shipmentDateUpdate(no,shipment_due_date);
+	    	}
+	    	ArrayList<ClientOrderModel> list = clientOrderJdbc.getClientOrderLog(item_name);
+	    	model.addAttribute("clientOrderList",list);
+	    	model.addAttribute("item_name", item_name);
+			model.addAttribute("resultText",resultText);
+	        return "html/ShipmentManagement";
+	    }
 
-		    	//入力された番号が存在するか、既に確定されているか確認し、入荷確定処理を行う。
-		    	String resultText = itemLogic.checkItemNoLogic(Integer.parseInt(itemNo));
-		    	model.addAttribute("resultText", resultText);
-		        return "html/ArrivalManagement";
-		    }
+  //在庫の更新
+    @RequestMapping("InventryUpdate")
+  		public String inventoryadjustmentupdate(@RequestParam("item_no") String searchWord, @RequestParam("item_stock") String updateStock,Model model)
+    {
+  			ArrayList<InventoryModel> returnList = ItemLogic.getInventoryUpdate(searchWord,updateStock);
 
-	  //在庫の更新
-	    @RequestMapping("InventryUpdate")
-	  		public String inventoryadjustmentupdate(@RequestParam("item_no") String searchWord, @RequestParam("item_stock") String updateStock,Model model)
-	    {
-	  			ArrayList<InventoryModel> returnList = ItemLogic.getInventoryUpdate(searchWord,updateStock);
+  			//更新結果を渡す
+  			if(returnList.size()==0){
+  				model.addAttribute("searchList", null);
+  				model.addAttribute("resultText", "更新できませんでした。");
+  				return "html/InventoryAdjustment";
+  			}else{
+  				model.addAttribute("searchList", returnList);
+  			}
+  			model.addAttribute("resultText", "更新しました！");
+  			return "html/InventoryAdjustment";
+  		}
 
-	  			//更新結果を渡す
-	  			if(returnList.size()==0){
-	  				model.addAttribute("searchList", null);
-	  				model.addAttribute("resultText", "更新できませんでした。");
-	  				return "html/InventoryAdjustment";
-	  			}else{
-	  				model.addAttribute("searchList", returnList);
-	  			}
-	  			model.addAttribute("resultText", "更新しました！");
-	  			return "html/InventoryAdjustment";
-	  		}
+    //入荷確定処理
+    @RequestMapping("ArrivalDateUpdate")
+    public String arrivalDueDateUpdate(@RequestParam("itemNo") String itemNo, @RequestParam("searchItemName") String searchItemName, Model model) {
+    	//intに変換可能か確認する
+    	String noConfirmationStr = itemLogic.inputConfirmation(itemNo);
+    	if(noConfirmationStr.equals("数値を入力してください。")){
+    		model.addAttribute("resultText", noConfirmationStr);
+    		return "html/ArrivalManagement";
+    	}
+    	
+    	//入力された番号が存在するか、既に確定されているか確認し、入荷確定処理を行う。
+    	String resultText = itemLogic.checkItemNoLogic(Integer.parseInt(itemNo));
+    	model.addAttribute("resultText", resultText);
+    	
+    	ArrayList<VenderOrderModel> searchList = new ArrayList<VenderOrderModel>();
+    	System.out.println("searchItemName:"+searchItemName);
+    	if(searchItemName.equals("")) {
+    	}else{
+    		searchList = venderOrderLogic.getVenderOrderLog(searchItemName);
+    		model.addAttribute("searchItemName", searchItemName);
+    	}
+    	if(searchList.size()>0) {
+    		model.addAttribute("searchList", searchList);
+    	}
+        return "html/ArrivalManagement";
+    }
+    
+    //検索機能
+    @RequestMapping("ArrivalDataSearch")
+    public String arrivalDateSearch(@RequestParam("searchItemName") String searchItemName, Model model) {
+    	ArrayList<VenderOrderModel> searchList = venderOrderLogic.getVenderOrderLog(searchItemName);
+    	if(searchList.size()>0) {
+    		model.addAttribute("searchList", searchList);
+    	}
+    	model.addAttribute("resultText", "検索結果："+searchList.size()+"件");
+    	model.addAttribute("searchItemName", searchItemName);
+    	return "html/ArrivalManagement";
+    }
+
 
 
 }
