d79bc23da6328194d854aa52ff35e9be3f5f5e65
OkajimaAyaka <83321115+OkajimaAyaka@users.noreply.github.com>
2021-06-21 17:59:19 +0900

    QuotationManagementController.javaの編集。
    QuotationManagementController.javaの編集を行いました。

--- src/main/java/com/example/demo/controller/QuotationManagementController.java
+++ src/main/java/com/example/demo/controller/QuotationManagementController.java
@@ -103,55 +103,6 @@ public class QuotationManagementController  {
 		if(totalPrice==-1) {
 			model.addAttribute("resultText", "エラーが発生しました。");
 		}else {
-			int itemPrice = itemJdbc.getItemData(no).getItemPrice();
-			model.addAttribute("itemNo", itemNo);
-			model.addAttribute("itemBuyCount", itemBuyCount);
-			model.addAttribute("totalPrice", itemPrice);
-			model.addAttribute("resultText", "見積内容をご確認ください。");
-		}
-		 
-		 return "html/QuotationInput";
-	 }
-	 
-	 //入力情報を確認し、合計金額を表示
-	 @RequestMapping("Quotationcheck")
-	 public String quotationcheck(@RequestParam("itemNo") String itemNo, @RequestParam("itemBuyCount") String itemBuyCount, Model model) {
- 		if(session.getAttribute("data") == null) {
-			message ="IDとパスワードを入力してください";
-	    		model.addAttribute("indexForm", new LoginModel());
-	    		model.addAttribute("message" , message);
-	    		return "html/Login";
-		}
-		 int no = -1;
-		 int buyCount = -1;
-		 //入力可能な数値であるか確認
-		 ArrayList<InventoryModel> itemList = itemJdbc.getItemDataList();;
-		 model.addAttribute("list", itemList);
-		 String returnText  = itemLogic.inputConfirmation(itemNo, itemBuyCount);
-		 if(returnText.equals("true")) {
-			 no = Integer.parseInt(itemNo);
-			 buyCount = Integer.parseInt(itemBuyCount);
-		 }else {
-			 model.addAttribute("resultText", returnText);
-			 return "html/QuotationInput";
-		 }
-		 
-		int totalItemNo =itemJdbc.getItemDataList().size();
-		if(totalItemNo<no) {
-			model.addAttribute("resultText", "入力された番号は存在しません。");
-			return "html/QuotationInput";
-		}
-		
-		int itemStock = itemJdbc.getItemData(no).getItemStock();
-		if(itemStock<buyCount) {
-			model.addAttribute("resultText", "購入希望数が在庫数を超えています。");
-			return "html/QuotationInput";
-		}
-		
-		int totalPrice = quotationLogic.getQuotationTotalPrice(no, buyCount);
-		if(totalPrice==-1) {
-			model.addAttribute("resultText", "エラーが発生しました。");
-		}else {
 			model.addAttribute("itemNo", itemNo);
 			model.addAttribute("itemBuyCount", itemBuyCount);
 			model.addAttribute("totalPrice", totalPrice);
@@ -174,7 +125,7 @@ public class QuotationManagementController  {
 		 int buyCount = Integer.parseInt(itemBuyCount);
 		 String resultText = quotationLogic.quotationSaveLogic(no, buyCount);
 		 model.addAttribute("resultText", resultText);
-		 ArrayList<InventoryModel> itemList = itemJdbc.getItemDataList();;
+		 ArrayList<InventoryModel> itemList = itemJdbc.getItemDataList();
 		 model.addAttribute("list", itemList);
 		 return "html/QuotationInput";
 	 }
@@ -219,7 +170,5 @@ public class QuotationManagementController  {
 		 }
 		 return "html/QuotationSearch";
 	 }
-
+	
 }
-
-
