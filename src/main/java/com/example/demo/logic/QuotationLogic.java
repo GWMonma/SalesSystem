8095e5cc8de3a1ab24be2f12d7763c50346c3943
OkajimaAyaka <83321115+OkajimaAyaka@users.noreply.github.com>
2021-06-21 17:20:59 +0900

    QuotationLogic.javaの編集。
    QuotationLogic.javaの編集を行いました。

--- src/main/java/com/example/demo/logic/QuotationLogic.java
+++ src/main/java/com/example/demo/logic/QuotationLogic.java
@@ -1,5 +1,6 @@
 package com.example.demo.logic;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.jdbc.ItemJdbc;
 import com.example.demo.jdbc.QuotationJdbc;
+import com.example.demo.model.InventoryModel;
+import com.example.demo.model.QuotationModel;
 import com.example.demo.model.InventoryModel;
 
 @Service
@@ -42,6 +45,30 @@ public class QuotationLogic {
 				returnTotalPrice = itemData.getItemPrice()*itemBuyCount;
 			}
 			return returnTotalPrice;
+		}
+	
+	//見積情報を検索
+		public ArrayList<QuotationModel> quotationSearchListLogic(String... searchWord){
+			ArrayList<InventoryModel> searchNoList = new ArrayList<InventoryModel>();
+			//ユーザー番号を取得
+			ArrayList<QuotationModel> returnList = new ArrayList<QuotationModel>();
+			Map<String, Object> list = (Map<String, Object>) session.getAttribute("data");
+			int userNo = (int) list.get("user_no");
+				if(searchWord.length>0) {
+					//品番取得、合計金額計算
+					searchNoList = itemJdbc.getInventoryLog(searchWord[0]);
+					returnList = quotationJdbc.quotationSearchList(userNo, searchNoList);
+				}else if(searchWord.length==0){
+					//品番取得、合計金額計算
+					returnList = quotationJdbc.quotationSearchList(userNo, searchNoList);
+				}
+			return returnList;
+		}
+		
+	//見積情報を削除
+		public String quotationDeleteLogic(int quotationNo) {
+			String returntext = quotationJdbc.quotationDelete(quotationNo);
+			return returntext;
 		}
 
 }
