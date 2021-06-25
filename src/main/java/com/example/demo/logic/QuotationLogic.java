bdd1b97357b92dea54fb73985bce78fc55eb4c61
OkajimaAyaka <83321115+OkajimaAyaka@users.noreply.github.com>
2021-06-25 16:41:09 +0900

    QuotationLogic.javaの編集。
    QuotationLogic.javaの編集を行いました。。

--- src/main/java/com/example/demo/logic/QuotationLogic.java
+++ src/main/java/com/example/demo/logic/QuotationLogic.java
@@ -1,5 +1,6 @@
 package com.example.demo.logic;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -9,10 +10,16 @@ import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
+import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -91,6 +98,247 @@ public class QuotationLogic {
 		public String quotationDeleteLogic(int quotationNo) {
 			String returntext = quotationJdbc.quotationDelete(quotationNo);
 			return returntext;
+		}
+	
+		//見積書を出力
+		public String QuotationOutputLogic() {
+			//ユーザー番号を取得
+			Map<String, Object> list = (Map<String, Object>) session.getAttribute("data");
+			int userNo = (int) list.get("user_no");
+			//見積情報を取得
+			ArrayList<InventoryModel> searchNoList = itemJdbc.getInventoryLog("");
+			ArrayList<QuotationModel> quotationList = quotationJdbc.quotationSearchList(userNo, searchNoList);
+			//現在の日付を取得
+			Date date = new Date();
+			DateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日");
+			String nowDate = dateFormat.format(date);
+			//合計金額を計算
+			int QuotationTotalPrice = 0;
+			for(QuotationModel quotationData: quotationList) {
+				QuotationTotalPrice += quotationData.getTotalPrice();
+			}
+			//ワークブックの作成
+			Workbook workbook1 = new XSSFWorkbook();
+			//シートの作成
+			Sheet sheet = workbook1.createSheet("見積書");
+			//セルの結合
+			sheet.addMergedRegion(new CellRangeAddress(2, 3, 2, 6));
+			sheet.addMergedRegion(new CellRangeAddress(4, 4, 2, 4));
+			sheet.addMergedRegion(new CellRangeAddress(4, 4, 5, 6));
+			sheet.addMergedRegion(new CellRangeAddress(5, 5, 2, 4));
+			sheet.addMergedRegion(new CellRangeAddress(7, 7, 2, 4));
+			//フォント タイトル
+			Font fontTitle = workbook1.createFont();
+			fontTitle.setFontHeightInPoints((short)24);
+			fontTitle.setFontName("游ゴシック");
+			//縦横 中央揃え、フォントサイズ 24pt
+			CellStyle styleTitle = workbook1.createCellStyle();
+			styleTitle.setVerticalAlignment(VerticalAlignment.CENTER);
+			styleTitle.setAlignment(HorizontalAlignment.CENTER);
+			styleTitle.setFont(fontTitle);
+			//縦横 中央揃えと格子 細線
+			CellStyle styleCenterGrid = workbook1.createCellStyle();
+			styleCenterGrid.setVerticalAlignment(VerticalAlignment.CENTER);
+			styleCenterGrid.setAlignment(HorizontalAlignment.CENTER);
+			styleCenterGrid.setBorderTop(BorderStyle.THIN);
+			styleCenterGrid.setBorderBottom(BorderStyle.THIN);
+			styleCenterGrid.setBorderLeft(BorderStyle.THIN);
+			styleCenterGrid.setBorderRight(BorderStyle.THIN);
+			//縦 中央揃えと格子 細線
+			CellStyle styleVerticalCenterGrid = workbook1.createCellStyle();
+			styleVerticalCenterGrid.setVerticalAlignment(VerticalAlignment.CENTER);
+			styleVerticalCenterGrid.setBorderTop(BorderStyle.THIN);
+			styleVerticalCenterGrid.setBorderBottom(BorderStyle.THIN);
+			styleVerticalCenterGrid.setBorderLeft(BorderStyle.THIN);
+			styleVerticalCenterGrid.setBorderRight(BorderStyle.THIN);
+			//縦 中央揃えと横 左揃え
+			CellStyle styleVerticalCenterLeft = workbook1.createCellStyle();
+			styleVerticalCenterLeft.setVerticalAlignment(VerticalAlignment.CENTER);
+			styleVerticalCenterLeft.setAlignment(HorizontalAlignment.LEFT);
+			//縦 中央揃えと横 右揃え
+			CellStyle styleVerticalCenterRight = workbook1.createCellStyle();
+			styleVerticalCenterRight.setVerticalAlignment(VerticalAlignment.CENTER);
+			styleVerticalCenterRight.setAlignment(HorizontalAlignment.RIGHT);
+			//横 中央揃え
+			CellStyle styleVerticalCenter = workbook1.createCellStyle();
+			styleVerticalCenter.setVerticalAlignment(VerticalAlignment.CENTER);
+			//上罫線 細線
+			CellStyle styleBorderTop = workbook1.createCellStyle();
+			styleBorderTop.setBorderTop(BorderStyle.THIN);
+			//左罫線 細線
+			CellStyle styleBorderLeft = workbook1.createCellStyle();
+			styleBorderLeft.setBorderLeft(BorderStyle.THIN);
+			//右罫線 細線
+			CellStyle styleBorderRight = workbook1.createCellStyle();
+			styleBorderRight.setBorderRight(BorderStyle.THIN);
+			//下罫線 細線
+			CellStyle styleBorderBottom = workbook1.createCellStyle();
+			styleBorderBottom.setBorderBottom(BorderStyle.THIN);
+			//上罫線と左罫線 細線
+			CellStyle styleBorderTopLeft = workbook1.createCellStyle();
+			styleBorderTopLeft.setBorderTop(BorderStyle.THIN);
+			styleBorderTopLeft.setBorderLeft(BorderStyle.THIN);
+			//上罫線と右罫線 細線
+			CellStyle styleBorderTopRight = workbook1.createCellStyle();
+			styleBorderTopRight.setBorderTop(BorderStyle.THIN);
+			styleBorderTopRight.setBorderRight(BorderStyle.THIN);
+			//下罫線と左罫線 細線
+			CellStyle styleBorderBottomLeft = workbook1.createCellStyle();
+			styleBorderBottomLeft.setBorderBottom(BorderStyle.THIN);
+			styleBorderBottomLeft.setBorderLeft(BorderStyle.THIN);
+			//下罫線と右罫線 細線
+			CellStyle styleBorderBottomRight = workbook1.createCellStyle();
+			styleBorderBottomRight.setBorderBottom(BorderStyle.THIN);
+			styleBorderBottomRight.setBorderRight(BorderStyle.THIN);
+			int forNo = 30;
+			//行を作成 日付や名前を表示するために必要な行数+見積情報数
+			for(int no = 0;no <= forNo; no++) {
+				Row row = sheet.createRow(no);
+				Cell cell;
+				if(no == 0) {
+					continue;
+				}
+				cell = row.createCell(1);
+				cell.setCellStyle(styleBorderLeft);
+				cell = row.createCell(7);
+				cell.setCellStyle(styleBorderRight);
+				if(no<=8) {
+					switch(no) {
+					//セルを生成し、入力
+					case 0:
+						continue;
+					case 1:
+						cell = row.createCell(1);
+						cell.setCellStyle(styleBorderTopLeft);
+						for(int cellNo = 2; cellNo < 7; cellNo++) {
+							cell = row.createCell(cellNo);
+							cell.setCellStyle(styleBorderTop);
+						}
+						cell = row.createCell(7);
+						cell.setCellStyle(styleBorderTopRight);
+						continue;
+					case 2://見積書
+						cell = row.createCell(1);
+						cell.setCellStyle(styleBorderLeft);
+						cell = row.createCell(2);
+						cell.setCellStyle(styleTitle);
+						cell.setCellValue("見積書");
+						cell = row.createCell(7);
+						cell.setCellStyle(styleBorderRight);
+						continue;
+					case 3:
+						continue;
+					case 4://ユーザー名
+						cell = row.createCell(2);
+						cell.setCellStyle(styleVerticalCenterLeft);
+						cell.setCellValue(list.get("user_id")+"様");
+						cell = row.createCell(5);
+						cell.setCellStyle(styleVerticalCenterRight);
+						cell.setCellValue("株式会社グッドハウス");
+						continue;
+					case 5://日付
+						cell = row.createCell(2);
+						cell.setCellStyle(styleVerticalCenterLeft);
+						cell.setCellValue("作成日 "+nowDate);
+						continue;
+					case 6:
+						continue;
+					case 7:
+						cell = row.createCell(2);
+						cell.setCellStyle(styleVerticalCenterLeft);
+						cell.setCellValue("合計金額 \\"+QuotationTotalPrice);
+						continue;
+					case 8:
+						cell = row.createCell(2);
+						cell.setCellStyle(styleCenterGrid);
+						cell.setCellValue("見積番号");
+						cell = row.createCell(3);
+						cell.setCellStyle(styleCenterGrid);
+						cell.setCellValue("商品名");
+						cell = row.createCell(4);
+						cell.setCellStyle(styleCenterGrid);
+						cell.setCellValue("品番");
+						cell = row.createCell(5);
+						cell.setCellStyle(styleCenterGrid);
+						cell.setCellValue("購入数");
+						cell = row.createCell(6);
+						cell.setCellStyle(styleCenterGrid);
+						cell.setCellValue("金額");
+						continue;
+					}
+				}
+				if(no-9 < quotationList.size()) {
+					cell = row.createCell(2);
+					cell.setCellValue(no-8);
+					cell.setCellStyle(styleVerticalCenterGrid);
+					cell = row.createCell(3);
+					cell.setCellValue(quotationList.get(no-9).getItemName());
+					cell.setCellStyle(styleVerticalCenterGrid);
+					cell = row.createCell(4);
+					cell.setCellValue(quotationList.get(no-9).getItemProductNo());
+					cell.setCellStyle(styleVerticalCenterGrid);
+					cell = row.createCell(5);
+					cell.setCellValue(quotationList.get(no-9).getItemBuyCount());
+					cell.setCellStyle(styleVerticalCenterGrid);
+					cell = row.createCell(6);
+					cell.setCellValue(quotationList.get(no-9).getTotalPrice());
+					cell.setCellStyle(styleVerticalCenterGrid);
+					continue;
+				}
+				//データが無い場合 合計20件格子
+				if(no-9 < 20) {
+					for(int cellGridNo = 2; cellGridNo < 7; cellGridNo++) {
+						cell = row.createCell(cellGridNo);
+						cell.setCellStyle(styleVerticalCenterGrid);
+					}
+					continue;
+				}
+				
+				if(no == 30) {
+					cell = row.createCell(1);
+					cell.setCellStyle(styleBorderBottomLeft);
+					for(int cellNo = 2; cellNo < 7; cellNo++) {
+						cell = row.createCell(cellNo);
+						cell.setCellStyle(styleBorderBottom);
+					}
+					cell = row.createCell(7);
+					cell.setCellStyle(styleBorderBottomRight);
+					continue;
+				}
+				
+			}
+					
+			//ファイルの出力
+			FileOutputStream output = null;
+			String path = "C:\\Users\\Education\\Desktop\\sample\\Quotation.xlsx";
+				int pathNo = 1;
+				//ファイルの存在を確認する
+				File file = new File(path);
+				while(file.exists()) {
+					//存在する場合
+					path = "C:\\Users\\Education\\Desktop\\sample\\Quotation("+pathNo+").xlsx";
+					file = new File(path);
+					pathNo++;
+				}
+					
+				try{
+					output = new FileOutputStream(path);
+					//書き込み
+					workbook1.write(output);
+				}catch(Exception ex){
+					ex.printStackTrace();
+					System.out.println("エラー1");
+				}finally {
+					try {
+						if(workbook1!=null) {workbook1.close();}
+						if(output!=null) {output.close();}
+					}catch(Exception ex2) {
+						ex2.printStackTrace();
+						System.out.println("エラー2");
+					}
+				}
+			
+			return "見積書の保存が完了しました。";
 		}
 
 }
