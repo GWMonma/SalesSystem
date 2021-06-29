eb07b1e1bb1ea2e0d9a736eb73940451f814cfe0
OkajimaAyaka <83321115+OkajimaAyaka@users.noreply.github.com>
2021-06-29 11:03:53 +0900

    VenderStockManagementController.javaの編集。
    VenderStockManagementController.javaの編集を行いました。

--- src/main/java/com/example/demo/controller/VenderStockManagementController.java
+++ src/main/java/com/example/demo/controller/VenderStockManagementController.java
@@ -1,7 +1,8 @@
 package com.example.demo.controller;
 
-
 import java.util.ArrayList;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,23 +11,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.demo.jdbc.ItemJdbc;
 import com.example.demo.jdbc.VenderOrderJdbc;
+import com.example.demo.model.LoginModel;
 import com.example.demo.model.VenderOrderModel;
 
 /*仕入れ管理関係のコントローラー*/
 @Controller
 public class  VenderStockManagementController  {
+	String message;
+	@Autowired
+	HttpSession session;
 	 @Autowired
 	 private VenderOrderJdbc venderOrderJdbc;
-	 
-	 @Autowired
-	 private ItemJdbc itemJdbc;
 
 	 
 	 //仕入管理画面表示
 	    @GetMapping("/VenderStockManagement")
-	    public String venderStockManagement() {
+	    public String venderStockManagement(Model model) {
+    		if(session.getAttribute("data") == null) {
+    			message ="IDとパスワードを入力してください";
+    	    		model.addAttribute("indexForm", new LoginModel());
+    	    		model.addAttribute("message" , message);
+    	    		return "html/Login";
+    		}
 			return "html/VenderStockManagement";
 		}
 	    
@@ -34,6 +41,12 @@ public class  VenderStockManagementController  {
 	 //フォームから入力された値を受け取りデータベースと照合
 	    @RequestMapping("VenderSearch")
 	    public String venderSearch(@RequestParam("item_name") String item_name, Model model){
+    		if(session.getAttribute("data") == null) {
+    			message ="IDとパスワードを入力してください";
+    	    		model.addAttribute("indexForm", new LoginModel());
+    	    		model.addAttribute("message" , message);
+    	    		return "html/Login";
+    		}
 	    	ArrayList<VenderOrderModel> list = venderOrderJdbc.getVenderOrderLog(item_name);
 	    	if(list.size()>0) {
 				model.addAttribute("venderOrderList",list);
@@ -48,6 +61,12 @@ public class  VenderStockManagementController  {
 	 //日付の更新
 	    @RequestMapping("VenderOrderDateUpdate")
 	    public String venderOrderDateUpdate(@RequestParam("item_name") String item_name, @RequestParam("date_update") String vender_order_no, Model model){
+    		if(session.getAttribute("data") == null) {
+    			message ="IDとパスワードを入力してください";
+    	    		model.addAttribute("indexForm", new LoginModel());
+    	    		model.addAttribute("message" , message);
+    	    		return "html/Login";
+    		}
 	    	int totalItemNo = venderOrderJdbc.getVenderOrderDataList().size();
 	    	String resultText = null;
 	    	int no = -1;
@@ -69,9 +88,5 @@ public class  VenderStockManagementController  {
 	    	model.addAttribute("item_name", item_name);
 	        return "html/VenderStockManagement";
 	    }
-
-	   
-
 	   
 	}
-
